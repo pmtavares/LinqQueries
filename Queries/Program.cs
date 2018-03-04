aeb375bbd03448fe404773593ac96bb47b168b68
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-04 21:29:16 +0000

    Add project files.

---
+++ Queries/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new PlutoContext();
+
+            var courses = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
+
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Name);
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+
+        }
+    }
+}
