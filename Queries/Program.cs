32e285b06b43790add9e56492748c07f04cc7f5c
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-05 21:35:07 +0000

    Linq Example

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -17,6 +17,31 @@ namespace Queries
             {
                 Console.WriteLine(c.Name);
             }
+            Console.WriteLine("### Courses of Level one ###");
+
+            var courses2 = context.Courses.Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenByDescending(c => c.Level);
+                
+
+            foreach (var c in courses2)
+            {
+                Console.WriteLine(string.Format("Course: {0}- Level: {1}",c.Name,c.Level));
+            }
+
+            Console.WriteLine("### Courses  ###");
+
+            var tags = context.Courses.Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenByDescending(c => c.Level)
+                .Select(c => c.Author)
+                .Distinct();
+
+            foreach (var t in tags)
+            {
+                Console.WriteLine(t.Name);
+            }
+
             //Make the window not close
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
